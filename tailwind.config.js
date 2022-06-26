a7c0c8034ece11d0986b0029ff03f72bf5b70c7c
Kim Janssens <kim.janssens@viavictor.com>
2022-06-26 13:56:54 +0200

    Innitial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				raleway: ["Raleway", "sans-serif"],
+			},
+			height: {
+				'screen-1/2': '50vh'
+			}
+		},
+	},
+	plugins: [],
+};
