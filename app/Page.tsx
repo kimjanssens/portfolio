67c42ac83c246103ed0a6e37dcb4a1fbaed22143
Kim Janssens <kim.janssens@viavictor.com>
2023-07-29 10:34:17 +0200

    Upgrade node version and run new next version

---
+++ app/Page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/hero";
+import Contact from "@/components/contact";
+
+export default function Page() {
+	return (
+		<>
+			<Hero />
+
+			<Contact />
+		</>
+	);
+}
