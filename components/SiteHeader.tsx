67805730f54c98b819572242742ff8669368d6fd
Kim Janssens <kim.janssens@viavictor.com>
2023-08-04 15:17:24 +0200

    nl copy and metadata + responsive styling

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -4,21 +4,25 @@ import SocialsMedia from "./SocialMedia";
 
 export default function SiteHeader() {
 	return (
-		<header className="max-h-screen px-8 py-24 flex flex-col justify-between">
+		<header className="lg:max-h-screen px-8 py-12 lg:py-24 flex flex-col justify-between">
 			<div className="flex flex-col">
 				<h1 className="text-4xl sm:text-5xl font-semibold tracking-tight mb-4">
 					Kim Janssens
 				</h1>
 				<h1 className="text-xl sm:text-2xl font-medium tracking-tight mb-8">
-					Front-end developer at Ellipsis Agency
+					Front-end developer bij Ellipsis Agency
 				</h1>
 				<p className="text-base sm:text-lg mb-8">
-					Freelance JavaScript developer with a focus on React. PHP developer
+					Freelance JavaScript developer met een focus op React. PHP developer
+					met een verleden in Drupal. Webdesigner. Passie voor sport, films en
+					games. Momenteel woonachtig in Tisselt, België.
+					{/* @TODO: Multilingual */}
+					{/* Freelance JavaScript developer with a focus on React. PHP developer
 					with a history in Drupal. Webdesigner. Passion for sports, movies and
-					games. Currently residenting in Tisselt, Belgium.
+					games. Currently residenting in Tisselt, Belgium. */}
 				</p>
 
-				<nav className="flex flex-col items-center justify-items-start w-full sm:flex-row">
+				<nav className="flex flex-col items-start w-full sm:flex-row mb-8">
 					<Link
 						href="mailto:hello@kimjanssens.be"
 						className="text-base sm:text-lg uppercase text-black block border-2 border-solid border-black px-4 py-2 hover:bg-black hover:text-white ease-in-out duration-300 sm:mr-4 mb-4 w-full sm:w-auto text-center"
@@ -30,7 +34,7 @@ export default function SiteHeader() {
 						target="_blank"
 						rel="noopener noreferrer"
 						download
-						className="text-base sm:text-lg uppercase text-black block border-2 border-solid border-black px-4 py-2 hover:bg-black hover:text-white ease-in-out duration-300 sm:mr-4 mb-4 w-full sm:w-auto text-center"
+						className="text-base sm:text-lg uppercase text-black block border-2 border-solid border-black px-4 py-2 hover:bg-black hover:text-white ease-in-out duration-300 sm:mr-4 w-full sm:w-auto text-center"
 					>
 						Curriculum Vitae
 					</Link>
