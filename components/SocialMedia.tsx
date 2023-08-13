1af208b5b1dc0e2eaa881d79ebdb93535011655c
Kim Janssens <kim.janssens@viavictor.com>
2023-08-13 19:08:25 +0200

    Add Google Analytics events

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { FiGithub, FiLinkedin, FiInstagram, FiTwitter } from "react-icons/fi";
 
+import { event } from "@/lib/gtagHelper";
+
 export default function SocialsMedia() {
 	return (
 		<nav className="flex items-center">
@@ -8,6 +10,13 @@ export default function SocialsMedia() {
 				href="https://github.com/kimjanssens"
 				target="_blank"
 				className="p-3 hover:-translate-y-1 transition ease-in-out duration-300"
+				onClick={() =>
+					event({
+						action: "click",
+						category: "Socials",
+						label: "Github",
+					})
+				}
 			>
 				<FiGithub className="h-5 w-5" />
 			</Link>
@@ -15,6 +24,13 @@ export default function SocialsMedia() {
 				href="https://www.instagram.com/kimjanssens90"
 				target="_blank"
 				className="p-3 hover:-translate-y-1 transition ease-in-out duration-300"
+				onClick={() =>
+					event({
+						action: "click",
+						category: "Socials",
+						label: "Instagram",
+					})
+				}
 			>
 				<FiInstagram className="h-5 w-5 text-black" />
 			</Link>
@@ -22,6 +38,13 @@ export default function SocialsMedia() {
 				href="https://twitter.com/kimjanssens90"
 				target="_blank"
 				className="p-3 hover:-translate-y-1 transition ease-in-out duration-300"
+				onClick={() =>
+					event({
+						action: "click",
+						category: "Socials",
+						label: "Twitter",
+					})
+				}
 			>
 				<FiTwitter className="h-5 w-5 text-black" />
 			</Link>
@@ -29,6 +52,13 @@ export default function SocialsMedia() {
 				href="https://www.linkedin.com/in/kimjanssens90"
 				target="_blank"
 				className="p-3 hover:-translate-y-1 transition ease-in-out duration-300"
+				onClick={() =>
+					event({
+						action: "click",
+						category: "Socials",
+						label: "LinkedIn",
+					})
+				}
 			>
 				<FiLinkedin className="h-5 w-5 text-black" />
 			</Link>
--- lib/gtagHelper.tsx
+++ lib/gtagHelper.tsx
@@ -3,3 +3,22 @@ export const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
 		page_path: url,
 	});
 };
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({
+	action,
+	category,
+	label,
+	value,
+}: {
+	action: string;
+	category: string;
+	label: string;
+	value?: number;
+}) => {
+	(window as any).gtag("event", action, {
+		event_category: category,
+		event_label: label,
+		value,
+	});
+};
