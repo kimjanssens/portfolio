e2fdb0a6d4ed2048824985f5730a4841ff7d02cf
Kim Janssens <kim.janssens@viavictor.com>
2023-12-01 09:18:31 +0100

    Add projects data

---
+++ app/_data.tsx
@@ -0,0 +1,38 @@
+export const CASES = [
+	{
+		title: "Jungle Wood",
+		image: {
+			src: "/cases/jungle-wood.png",
+			alt: "Jungle Wood website",
+		},
+		tags: ["Next.js", "Vercel", "TypeScript", "Tailwind CSS"],
+		website: "https://jungle-wood.be/",
+	},
+	{
+		title: "Thuisverpleging Lathouwers-Huysmans",
+		image: {
+			src: "/cases/thuisverpleging-lathouwers.png",
+			alt: "Thuisverpleging Lathouwers-Huysmans website",
+		},
+		tags: ["Next.js", "Vercel", "TypeScript", "Tailwind CSS"],
+		website: "https://thuisverpleging-lathouwers.be/",
+	},
+	{
+		title: "Car Perfection",
+		image: {
+			src: "/cases/car-perfection.png",
+			alt: "Car Perfection website",
+		},
+		tags: ["Next.js", "Vercel", "TypeScript", "Tailwind CSS"],
+		website: "https://carperfection.be/",
+	},
+	{
+		title: "Huis MIO",
+		image: {
+			src: "/cases/huis-mio.png",
+			alt: "Huis MIO website",
+		},
+		tags: ["Drupal 9", "SCSS"],
+		website: "http://huismio.be/",
+	},
+];
