9190413be068fcbb9dd8adf7ae71f49327eae99b
Kim Janssens <kimjanssens21@gmail.com>
2024-09-06 22:44:51 +0200

    basic header + install tailwind

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,14 @@
 <template>
-	<header>HEADER</header>
+	<header class="py-2">
+		<div class="container mx-auto flex items-center justify-between">
+			<AppLogo />
+
+			<nav class="flex items-center gap-4">
+				<nuxt-link to="/">Home</nuxt-link>
+				<nuxt-link to="/about">About</nuxt-link>
+				<nuxt-link to="/services">Services</nuxt-link>
+				<nuxt-link to="/contact">Contact</nuxt-link>
+			</nav>
+		</div>
+	</header>
 </template>
---
+++ components/AppLogo.vue
@@ -0,0 +1,11 @@
+<template>
+	<nuxt-link to="/">
+		<h1 class="text-4xl font-black -tracking-wider">
+			Kimjanssens<span class="text-green-400">.be</span>
+		</h1>
+		<h2 class="text-xs font-light -tracking-wider text-green-400">
+			Digital Agency
+		</h2>
+	</nuxt-link>
+</template>
+s
